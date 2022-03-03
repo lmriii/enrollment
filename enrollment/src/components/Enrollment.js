b1c149d4d65d48a546e4dd31af7a115da76d0296
Michael R <mrwolf.lmriii@gmail.com>
2022-03-02 21:57:01 -0700

    Creating form

--- enrollment/src/components/Enrollment.js
+++ enrollment/src/components/Enrollment.js
@@ -6,39 +6,181 @@ export default class Example extends React.Component {
         return (
             <>
                 <header>
-                    MIDTOWN PRIMARY SCHOOL<br />
-                    Student Emergency Information
+                    <h1>MIDTOWN PRIMARY SCHOOL</h1>
+                    <h2>Student Emergency Information</h2>
                 </header>
+
                 <Form>
-                    <FormGroup>
-                        <Label for='name'>Student's Name</Label>
-                        <Input type='text' name='name' id='name' />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for='lastName'>Student's Last Name</Label>
-                        <Input type='text' name='lastName' id='lastName' />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="dob">Date of Birth</Label>
-                        <Input type="date" name="dob" id="dob" placeholder="date placeholder" />
-                    </FormGroup>
-                    
-                    <FormGroup>
-                        <Label for='name'>Student Name</Label>
-                        <Input type='text' name='name' id='name' />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for='name'>Student Name</Label>
-                        <Input type='text' name='name' id='name' />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for='name'>Student Name</Label>
-                        <Input type='text' name='name' id='name' />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for='name'>Student Name</Label>
-                        <Input type='text' name='name' id='name' />
-                    </FormGroup>
+                    <div style={{
+                        borderWidth: '10px',
+                        borderStyle: 'solid'
+                    }}>
+                        <FormGroup>
+                            <Label for='name'>Student's Name</Label>
+                            <Input type='text' name='name' id='name' />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='lastName'>Student's Last Name</Label>
+                            <Input type='text' name='lastName' id='lastName' />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="dob">Date of Birth</Label>
+                            <Input type="date" name="dob" id="dob" placeholder="date placeholder" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='phone'>Phone</Label>
+                            <Input type='number' name='phone' id='phone' />
+                        </FormGroup>
+                        <FormGroup required check>
+                            <Label check>
+                                <Input type="radio" name="radio1" />{' '}
+                                Re-enrollment
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="radio" name="radio1" />{' '}
+                                Enrollment
+                            </Label>
+                        </FormGroup>
+                    </div>
+                    <h2>Parent's Information</h2>
+                    <div style={{
+                        borderWidth: '10px',
+                        borderStyle: 'solid'
+                    }}>
+
+                        <div>
+                            <h3>Mother's Data</h3>
+                            <FormGroup>
+                                <Label for='momthersName'>Mother's Name</Label>
+                                <Input type='text' name='mothersName' id='mothersName' />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='mothersLastName'>Mother's Last Name</Label>
+                                <Input type='text' name='mothersLastName' id='mothersLastName' />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='momAddress'>Mother's Home Address (if different)</Label>
+                                <Input type='textarea' name='momAddress' id='momAddress' />
+                            </FormGroup>
+                            <h3 style={{
+                                borderTopWidth: '10px',
+                                borderTopStyle: 'solid'
+                            }}>Father's Data</h3>
+                            <FormGroup>
+                                <Label for='dadName'>Mother's Name</Label>
+                                <Input type='text' name='dadName' id='dadName' />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='dadLastName'>Mother's Last Name</Label>
+                                <Input type='text' name='dadLastName' id='dadLastName' />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='dadAddress'>Mother's Home Address (if different)</Label>
+                                <Input type='textarea' name='dadAddress' id='dadAddress' />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='dadPhone1'>Home Phone</Label>
+                                <Input type='number' name='dadPhone1' id='dadPhone1' />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='dadPhone2'>Cell Phone</Label>
+                                <Input type='number' name='dadPhone2' id='dadPhone2' />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='dadPhone3'>Work Phone</Label>
+                                <Input type='number' name='dadPhone3' id='dadPhone3' />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='dadCompanyAddress'>Company & Address</Label>
+                                <Input type='textarea' name='dadCompanyAddress' id='dadCompanyAddress' />
+                            </FormGroup>
+                        </div>
+                    </div>
+                    <h3>Emergency Contact Numbers</h3>
+
+                    <div style={{
+                        borderWidth: '10px',
+                        borderStyle: 'solid'
+                    }}>
+                        <h4>Contact 1</h4>
+                        <FormGroup>
+                            <Label for='contact1'>Name</Label>
+                            <Input type='text' name='contact1' id='contact1' />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='relationship'>Relationship</Label>
+                            <Input type='text' name='relationship' id='relationship' />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='contact1Address'>Address </Label>
+                            <Input type='textarea' name='contact1Address' id='contact1Address' />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='contact1Phone1'>Home Phone</Label>
+                            <Input type='number' name='contact1Phone1' id='contact1Phone1' />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='contact1Phone2'>Cell Phone</Label>
+                            <Input type='number' name='contact1Phone2' id='contact1Phone2' />
+                        </FormGroup>
+                        <hr />
+                        <h4>Contact 2</h4>
+                        <FormGroup>
+                            <Label for='contact2'>Name</Label>
+                            <Input type='text' name='contact2' id='contact2' />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='relationship2'>Relationship</Label>
+                            <Input type='text' name='relationship2' id='relationship2' />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='contact2Address'>Address </Label>
+                            <Input type='textarea' name='contact2Address' id='contact2Address' />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='contact2Phone1'>Home Phone</Label>
+                            <Input type='number' name='contact2Phone1' id='contact2Phone1' />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for='contact2Phone2'>Cell Phone</Label>
+                            <Input type='number' name='contact2Phone2' id='contact2Phone2' />
+                        </FormGroup>
+                        <p>If I cannot be reached, the above-named people may be contacted and are authorized to pick up my child.</p>
+                        <FormGroup>
+                            <Label for='initials'>Initials</Label>
+                            <Input type='text' name='initials' id='initials' />
+                        </FormGroup>
+                        <FormGroup required check>
+                            <Label check>
+                                <Input type="radio" name="radio1" />{' '}
+                                OK
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input type="radio" name="radio1" />{' '}
+                                Not-OK
+                            </Label>
+                        </FormGroup>
+                    </div>
+                    <h3>Special Instructions</h3>
+                    <div style={{
+                        borderWidth: '10px',
+                        borderStyle: 'solid'
+                    }}>
+<FormGroup>
+                                <Label for='allergies'>Allergies or Special Conditions </Label>
+                                <Input type='textarea' name='allergies' id='allergies' />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='notAllowed'>Anyone NOT allowed to pick up student </Label>
+                                <Input type='textarea' name='notAllowed' id='notAllowed' />
+                            </FormGroup>
+
+                    </div>
+
                     <FormGroup>
                         <Label for='name'>Student Name</Label>
                         <Input type='text' name='name' id='name' />
